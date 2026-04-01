b615361768ebe33968c43aae720a7e21c635b108
mrjamiebowman <jamie@redoma.digital>
2026-03-31 23:13:14 -0500

    TSA, BaggageCart, Schema builder.

---
+++ src/TerribleSettingsAuditor.Abstractions/Attribute/BaggageCart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TerribleSettingsAuditor.Abstractions.Attribute;
+
+/// <summary>
+///  BagggeCart, is used to generate configuration files.
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class BaggageCart : System.Attribute
+{
+    public string Name { get; set; }
+
+    public string DefaultFileName { get; set; } = "appsettings.tsa.json";
+
+    public BaggageCart(string name)
+    {
+        Name = name;
+    }
+
+    public BaggageCart(string name, string fileName)
+    {
+        Name = name;
+        DefaultFileName = fileName;
+    }
+}
